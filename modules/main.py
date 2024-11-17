ab00ddb551d37b102448c677b1b3fd9ab6f4eb5c
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2024-11-17 07:09:54 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -50,7 +50,6 @@ async def stop_bot():
     await bot.stop()
 
 async def main():
-    if
         # Start the web server
         app_runner = web.AppRunner(await web_server())
         await app_runner.setup()
